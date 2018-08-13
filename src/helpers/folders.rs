36b69247651209f2c1682b3ddfebbb1bd4003234
Boris <ttyv00@gmail.com>
2018-08-14 02:06:19 +0300

    Add folder macros for OrderedFloat + fold some more functions

--- src/helpers/folders.rs
+++ src/helpers/folders.rs
@@ -1,20 +1,44 @@
-macro_rules! expander {
+macro_rules! fold {
     ($rust_func_name:ident, $( ($f_name:ident, $t_name:ty) ),+) => {
         $(
             crate fn $f_name(py: Python<'_>, xs: PyObject) -> PyResult<$t_name> {
-                let ys: Vec<$t_name> = pylist_to_vec(py, xs)?;
+                let ys = pylist_to_vec(py, xs)?;
                 to_python_result(py, stat_funcs::$rust_func_name(ys))
             }
         )+
     }
 }
 
-macro_rules! expander_mut {
+macro_rules! fold_mut {
     ($rust_func_name:ident, $( ($f_name:ident, $t_name:ty) ),+) => {
         $(
             crate fn $f_name(py: Python<'_>, xs: PyObject) -> PyResult<$t_name> {
-                let mut ys: Vec<$t_name> = pylist_to_vec(py, xs)?;
+                let mut ys = pylist_to_vec(py, xs)?;
                 to_python_result(py, stat_funcs::$rust_func_name(&mut ys))
+            }
+        )+
+    }
+}
+
+macro_rules! fold_ordered_float {
+    ($rust_func_name:ident, $( ($f_name:ident, $t_name:ty) ),+) => {
+        $(
+            crate fn $f_name(py: Python<'_>, xs: PyObject) -> PyResult<$t_name> {
+                let ys = extract_ordered_floats(py, &xs)?;
+                let res = stat_funcs::$rust_func_name::<OrderedFloat<$t_name>>(ys);
+                to_python_result(py, res.map(|x| x.into()))
+            }
+        )+
+    }
+}
+
+macro_rules! fold_ordered_float_mut {
+    ($rust_func_name:ident, $( ($f_name:ident, $t_name:ty) ),+) => {
+        $(
+            crate fn $f_name(py: Python<'_>, xs: PyObject) -> PyResult<$t_name> {
+                let mut ys = extract_ordered_floats(py, &xs)?;
+                let res = stat_funcs::$rust_func_name::<OrderedFloat<$t_name>>(&mut ys);
+                to_python_result(py, res.map(|x| x.into()))
             }
         )+
     }
--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -9,7 +9,6 @@ use crate::stat_funcs::errors::to_python_result;
 
 use cpython::{FromPyObject, PyDrop, PyList, PyObject, PyResult, Python};
 
-
 #[inline]
 fn pylist_to_vec<T>(py: Python<'_>, xs: PyObject) -> PyResult<Vec<T>>
     where for<'a> T: FromPyObject<'a> {
@@ -17,8 +16,8 @@ fn pylist_to_vec<T>(py: Python<'_>, xs: PyObject) -> PyResult<Vec<T>>
 }
 
 #[inline]
-fn extract_ordered_floats<T>(py: Python<'_>, obj: &PyObject) -> PyResult<Vec<OrderedFloat<T>>> where
-        for<'a> T: Float + FromPyObject<'a> {
+fn extract_ordered_floats<T>(py: Python<'_>, obj: &PyObject) -> PyResult<Vec<OrderedFloat<T>>>
+    where for<'a> T: Float + FromPyObject<'a> {
 
     let list = try!(obj.cast_as::<PyList>(py));
 
@@ -34,83 +33,20 @@ fn extract_ordered_floats<T>(py: Python<'_>, obj: &PyObject) -> PyResult<Vec<Ord
     Ok(v)
 }
 
-expander!(variance, (variance_f64_py, f64), (variance_f32_py, f32));
+fold!(variance, (variance_f64_py, f64), (variance_f32_py, f32));
 
-expander!(mean, (mean_f64_py, f64), (mean_f32_py, f32));
+fold!(mean, (mean_f64_py, f64), (mean_f32_py, f32));
 
-expander!(harmonic_mean, (harmonic_mean_f64_py, f64), (harmonic_mean_f32_py, f32));
+fold!(harmonic_mean, (harmonic_mean_f64_py, f64), (harmonic_mean_f32_py, f32));
 
-expander_mut!(median, (median_f64_py, f64), (median_f32_py, i64));
+fold_mut!(median, (median_f64_py, f64), (median_f32_py, i64));
 
-// floats have to be converted to OrderedFloats explicitly,
-// therefor can't be expanded with macros
-crate fn median_low_f64_py(py: Python<'_>, xs: PyObject) -> PyResult<f64> {
-    let mut ys = extract_ordered_floats(py, &xs)?;
-    let res = match stat_funcs::median_low::<OrderedFloat<f64>>(&mut ys) {
-        Ok(res) => Ok(res.into()),
-        Err(err) => Err(err),
-    };
+fold_ordered_float_mut!(median_low, (median_low_f32_py, f32), (median_low_f64_py, f64));
+fold_ordered_float_mut!(median_high, (median_high_f32_py, f32), (median_high_f64_py, f64));
 
-    to_python_result(py, res)
-}
+fold_ordered_float!(mode, (mode_f32_py, f32), (mode_f64_py, f64));
 
-// floats have to be converted to OrderedFloats explicitly,
-// therefor can't be expanded with macros
-crate fn median_low_f32_py(py: Python<'_>, xs: PyObject) -> PyResult<f32> {
-    let mut ys = extract_ordered_floats(py, &xs)?;
-    let res = match stat_funcs::median_low::<OrderedFloat<f32>>(&mut ys) {
-        Ok(res) => Ok(res.into()),
-        Err(err) => Err(err),
-    };
-
-    to_python_result(py, res)
-}
-
-// floats have to be converted to OrderedFloats explicitly,
-// therefor can't be expanded with macros
-crate fn median_high_f64_py(py: Python<'_>, xs: PyObject) -> PyResult<f64> {
-    let mut ys = extract_ordered_floats(py, &xs)?;
-    let res = match stat_funcs::median_high::<OrderedFloat<f64>>(&mut ys) {
-        Ok(res) => Ok(res.into()),
-        Err(err) => Err(err),
-    };
-
-    to_python_result(py, res)
-}
-
-// floats have to be converted to OrderedFloats explicitly,
-// therefor can't be expanded with macros
-crate fn median_high_f32_py(py: Python<'_>, xs: PyObject) -> PyResult<f32> {
-    let mut ys = extract_ordered_floats(py, &xs)?;
-    let res = match stat_funcs::median_high::<OrderedFloat<f32>>(&mut ys) {
-        Ok(res) => Ok(res.into()),
-        Err(err) => Err(err),
-    };
-
-    to_python_result(py, res)
-}
-
-// floats have to be converted to OrderedFloats explicitly,
-// therefor can't be expanded with macros
-crate fn mode_f64_py(py: Python<'_>, xs: PyObject) -> PyResult<f64> {
-    let ys = extract_ordered_floats(py, &xs)?;
-    let res = match stat_funcs::mode::<OrderedFloat<f64>>(ys) {
-        Ok(res) => Ok(res.into()),
-        Err(err) => Err(err),
-    };
-    to_python_result(py, res)
-}
-
-crate fn mode_f32_py(py: Python<'_>, xs: PyObject) -> PyResult<f32> {
-    let ys = extract_ordered_floats(py, &xs)?;
-    let res = match stat_funcs::mode::<OrderedFloat<f32>>(ys) {
-        Ok(res) => Ok(res.into()),
-        Err(err) => Err(err),
-    };
-    to_python_result(py, res)
-}
-
-expander!(mode,
+fold!(mode,
          (mode_str_py, String), (mode_i64_py, i64), (mode_i32_py, i32), (mode_u64_py, u64),
          (mode_u32_py, u32));
 
