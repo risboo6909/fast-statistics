46499770d6ef6fc8ea36ab407393f16e0025fd31
risboo6909 <ttyv00@gmail.com>
2018-09-09 02:34:43 +0300

    Add setup.py and build-wheels.sh

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup
+
+try:
+    from setuptools_rust import RustExtension
+except ImportError:
+    import subprocess
+
+    errno = subprocess.call([sys.executable, "-m", "pip", "install", "setuptools-rust"])
+    if errno:
+        print("Please install setuptools-rust package")
+        raise SystemExit(errno)
+    else:
+        from setuptools_rust import RustExtension
+
+setup_requires = ["setuptools-rust>=0.10.1", "wheel"]
+install_requires = []
+
+setup(
+    name="fast-statistics",
+    version="0.1.0",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    packages=["fast_statistics"],
+    rust_extensions=[RustExtension("fast_statistics.fast_statistics")],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    include_package_data=True,
+    zip_safe=False,
+)
+
