e1dfa9617268c90fd080d05b0bd5c70145496412
risboo6909 <ttyv00@gmail.com>
2018-09-09 01:38:38 +0300

    Remove unused error types

--- src/stat_funcs/errors.rs
+++ src/stat_funcs/errors.rs
@@ -5,8 +5,6 @@ py_exception!(fast_stat, StatisticsError);
 
 #[derive(Fail, Debug)]
 crate enum MyError {
-    #[fail(display = "integer division or modulo by zero")]
-    ZeroDivisionError,
     #[fail(display = "harmonic_mean requires at least one data point")]
     HarmonicNoDataPoints,
     #[fail(display = "harmonic mean does not support negative values")]
@@ -22,12 +20,10 @@ crate enum MyError {
     NoMedianEmptyData,
     #[fail(display = "variance requires at least two data points")]
     NoEnoughDataForVariance,
-    #[fail(display = "population variance requires at least two data points")]
+    #[fail(display = "population variance requires at least one data point")]
     NoEnoughDataForPopulationVariance,
     #[fail(display = "mean requires at least one data point")]
     NoEnoughDataForMean,
-    #[fail(display = "wrong type")]
-    WrongTypeError,
 }
 
 #[inline]
