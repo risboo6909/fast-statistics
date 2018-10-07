c56385b3d247229ff32d4ef173657b60eae792f6
risboo6909 <ttyv00@gmail.com>
2018-10-08 02:00:46 +0300

    Speedup conversion into OrderedFloat

--- src/utils.rs
+++ src/utils.rs
@@ -1,31 +1,49 @@
 use num::Float;
+use core::mem::transmute;
 
-use cpython::{FromPyObject, PyDrop, PyList, PyObject, PyResult, Python};
-use ordered_float::OrderedFloat;
+use cpython::{FromPyObject, PyObject, PyResult, Python};
+use ordered_float::{OrderedFloat, NotNaN};
 
 #[inline]
 crate fn pylist_to_vec<T>(py: Python<'_>, xs: PyObject) -> PyResult<Vec<T>>
-where
-    for<'a> T: FromPyObject<'a>,
+    where
+            for<'a> T: FromPyObject<'a>,
 {
     Vec::extract(py, &xs)
 }
 
 #[inline]
-crate fn extract_ordered_floats<T>(py: Python<'_>, obj: &PyObject) -> PyResult<Vec<OrderedFloat<T>>>
-where
-    for<'a> T: Float + FromPyObject<'a>,
+crate fn into_mut_notnan<T>(xs: &mut [T]) -> &mut[NotNaN<T>]
+    where T: Float {
+    // very fast but unsafe conversion from slice of floats into slice of NotNaNs
+    unsafe { transmute::<&mut[T], &mut[NotNaN<T>]>(xs) }
+}
+
+#[inline]
+crate fn extract_ordered_floats<T>(py: Python<'_>, xs: &PyObject) -> PyResult<Vec<OrderedFloat<T>>>
+    where
+            for<'a> T: Float + FromPyObject<'a>,
 {
-    let list = obj.cast_as::<PyList>(py)?;
+    // very fast but unsafe conversion from slice of floats into slice of OrderedFloats
+    let ys = unsafe {
+        transmute::<&[T], &[OrderedFloat<T>]>(&Vec::extract(py, &xs)?)
+    };
 
-    let len = list.len(py);
-    let mut v = Vec::with_capacity(len);
+    Ok(ys.to_vec())
+}
 
-    for i in 0..len {
-        let item = list.get_item(py, i);
-        v.push(OrderedFloat(T::extract(py, &item)?));
-        item.release_ref(py);
-    }
 
-    Ok(v)
+#[test]
+fn test_extract_notnan() {
+    let mut xs = vec![1.0, 2.5, 3.7];
+    let ys = into_mut_notnan(&mut xs);
+
+    assert_eq!(ys,
+               [
+                   NotNaN::new(1.0).unwrap(),
+                   NotNaN::new(2.5).unwrap(),
+                   NotNaN::new(3.7).unwrap()
+               ]
+    );
+
 }
