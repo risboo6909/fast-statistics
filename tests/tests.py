679ad37725a7095b51f7836e45564f84091582e5
root <ttyv00@gmail.com>
2018-10-22 10:56:11 +0000

    Add tests for the rest of functions

--- tests/tests.py
+++ tests/tests.py
@@ -16,6 +16,13 @@ def test_with_msg(msg, test_func):
     print('ok')
 
 
+@given(data())
+def test_kth_stat(data):
+    xs = data.draw(lists(floats(allow_nan=False, allow_infinity=False), min_size=1))
+    k = data.draw(integers(min_value=0, max_value=len(xs)-1))
+    assert fast_stat.kth_elem_float(xs, k) == sorted(xs)[k]
+
+
 @given(lists(floats(allow_nan=False, allow_infinity=False), min_size=1))
 def test_median(xs):
     assert fast_stat.median(xs) == statistics.median(xs)
@@ -33,14 +40,51 @@ def test_median_high(xs):
 
 @given(data())
 def test_median_grouped(data):
-    xs = data.draw(lists(floats(allow_nan=False, allow_infinity=False), min_size=2))
-    interval = data.draw(integers(min_value=1, max_value=len(xs)-1))
+    xs = data.draw(lists(floats(allow_nan=False, allow_infinity=False), min_size=1))
+    interval = data.draw(integers(min_value=1, max_value=len(xs)))
     assert isclose(fast_stat.median_grouped(xs, interval), statistics.median_grouped(xs, interval)) is True
 
 
+# width=32 is to prevent floating point OverflowError
+@given(lists(floats(allow_nan=False, allow_infinity=False, width=32), min_size=2))
+def test_stdev(xs):
+    assert isclose(fast_stat.stdev(xs), statistics.stdev(xs)) is True
+
+
+# width=32 is to prevent floating point OverflowError
+@given(lists(floats(allow_nan=False, allow_infinity=False, width=32), min_size=2))
+def test_pstdev(xs):
+    assert isclose(fast_stat.pstdev(xs), statistics.pstdev(xs)) is True
+
+
+# width=32 is to prevent floating point OverflowError
+@given(lists(floats(allow_nan=False, allow_infinity=False, width=32), min_size=2))
+def test_variance(xs):
+    assert isclose(fast_stat.variance(xs), statistics.variance(xs)) is True
+
+
+# width=32 is to prevent floating point OverflowError
+@given(lists(floats(allow_nan=False, allow_infinity=False, width=32), min_size=2))
+def test_pvariance(xs):
+    assert isclose(fast_stat.pvariance(xs), statistics.pvariance(xs)) is True
+
+
+# width=32 is to prevent floating point OverflowError
+# TODO: check rust floats
+@given(lists(floats(allow_nan=False, allow_infinity=False, min_value=0.0, width=32), min_size=1))
+def test_harmonic_mean(xs):
+    assert isclose(fast_stat.harmonic_mean(xs), statistics.harmonic_mean(xs)) is True
+
+
 if __name__ == '__main__':
-    test_with_msg('Testing median function', test_median)
-    test_with_msg('Testing median_low function', test_median_low)
-    test_with_msg('Testing median_high function', test_median_high)
-    test_with_msg('Testing median_grouped function', test_median_grouped)
+    test_with_msg('Testing kth_stat', test_kth_stat)
+    test_with_msg('Testing median', test_median)
+    test_with_msg('Testing median_low', test_median_low)
+    test_with_msg('Testing median_high', test_median_high)
+    test_with_msg('Testing median_grouped', test_median_grouped)
+    test_with_msg('Testing stdev', test_stdev)
+    test_with_msg('Testing pstdev', test_pstdev)
+    test_with_msg('Testing variance', test_variance)
+    test_with_msg('Testing pvariance', test_pvariance)
+    test_with_msg('Testing harmonic_mean', test_harmonic_mean)
 
